bef34dfcdb48361883b3ff26e0417a90455b4bda
ram-maker <ramprasadpoudel150@gmail.com>
2025-01-02 23:04:33 +0545

    chore:performs median

--- DSA/arrays/ascending_sort.cpp
+++ DSA/arrays/ascending_sort.cpp
@@ -1,43 +1,46 @@
 #include <iostream>
 using namespace std;
-// int sortArray(int *array,int array_size){
-//     int *tmp=new int[array_size];
-//     tmp=array;
-//     int tmp_number;
-//     for(int i=0;i<array_size;i++){
-//         for (int j=1;j<array_size;j++){
-//             if (*(array+i)>*(array+j)){
-//                 tmp[i]=
-//             }
-//         }
-//     }
-// }
-
-int main(){
-    int array[]={50,23,500,28,190,1};
-    int size=6;
-    int tmp;
-    int loop_count=size-1;
-    for (int j=0;j<loop_count;j++){
-        for (int i=0;i<loop_count;i++){  
-            if (array[i]>array[i+1]){
+int *sortArray(int *array,int array_size){
+    int tmp=0;
+    for(int j=0;j<array_size-1;j++){
+        for (int i=0;i<array_size-1;i++){
+            if(array[i]>array[i+1]){
                 tmp=array[i];
                 array[i]=array[i+1];
                 array[i+1]=tmp;
             }
-            
-        }
-        cout<<j<<". ";
-            for (int i=0;i<6;i++){
-                cout<<array[i]<<" ";
-            }
-            cout<<endl;
-            // loop_count--;
-
+        }      
     }
-    // for (int i=0;i<6;i++){
-    //     cout<<array[i]<<" ";
-    // }
-    // cout<<endl;
+    int *sorted_array=new int[array_size];
+    sorted_array=array;
+    return sorted_array;
+}
+
+float calculate_median(int *ascending_array,int size){
+    float median=0;
+    int first_median_term=0;
+    int rem=size%2;
+    if (rem==0){
+       first_median_term=ascending_array[size/2-1];
+       int second_median_term=ascending_array[size/2]; 
+       median=static_cast<float>(first_median_term+second_median_term)/2;
+    }
+    else{
+        median=ascending_array[size/2];
+    }
+    return median;
+}
+
+int main(){
+    int array[]={50,23,0,28,190,1};
+    int size=sizeof(array)/sizeof(array[0]);
+    int *sorted_array=sortArray(array,size+1);
+    cout<<"The sorted array is:"<<endl;
+    for (int i=0;i<size;i++){
+        cout<<sorted_array[i]<<" ";
+    }
+    cout<<endl;
+    float median=calculate_median(sorted_array,size); 
+    cout<<"Median is:"<<median<<endl;
     return 0;
 }
