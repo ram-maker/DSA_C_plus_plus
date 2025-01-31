5a538c123c6cfc7218d812f80abbb613ee6a43cf
ram-maker <ramprasadpoudel150@gmail.com>
2025-01-31 19:12:16 +0545

    define function base object

--- oops/polymerphism.cpp
+++ oops/polymerphism.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 using namespace std;
+
 class Base{
     public:
         virtual void display(){
@@ -7,14 +8,25 @@ class Base{
         }
 };
 
-class Derive:public Base{
+class Derive1:public Base{
     void display(){
-        cout<<"This is derive class"<<endl;
+        cout<<"This is derive1 class"<<endl;
 
     }
 };
+class Derive2:public Base{
+    void display(){
+        cout<<"This is derive 2 class"<<endl;
+    }
+};
+
+void takesBaseObject(Base *base_obj){
+    base_obj->display();
+}
+
 int main(){
-    Base *base=new Derive();
-    base->display();
+    Base *base=new Derive1();
+    takesBaseObject(base);
+    // base->display();
     return 0;
 }
