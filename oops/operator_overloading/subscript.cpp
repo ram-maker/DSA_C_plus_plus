071d80babc9dc6af3e178ba97fb2db417baa3d8d
ram-maker <ramprasadpoudel150@gmail.com>
2025-02-01 10:22:34 +0545

    chore:overload subscript operator

---
+++ oops/operator_overloading/subscript.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+class Array{
+    int size;
+    int *arr_ptr;
+    public:
+    Array(){
+
+    }
+    Array(int *arr_ptr){
+        this->arr_ptr=arr_ptr;
+    }
+    int operator[](int index){
+        return *(arr_ptr+index);
+    }
+};
+
+int main(){
+    int some_array[]={9,0,8,7};
+    Array arr(some_array);
+    for(int i=0;i<4;i++){
+        cout<<arr[i]<<endl;
+    }
+}
