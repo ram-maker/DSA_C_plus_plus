f9abd30c0b30bb1956020e403f7a34f55eb143fe
ram-maker <ramprasadpoudel150@gmail.com>
2025-02-02 13:36:20 +0545

    chore:sorts lists

--- STL/list/basics..cpp
+++ STL/list/basics..cpp
@@ -11,11 +11,8 @@ void printList(list<int> given_list){
 int main(){
       list<int> some_list = {23, 34, 8, 9, 0};
     printList(some_list);
-
-    auto first_element = some_list.begin();
-    advance(first_element, 2); // Move iterator to the 3rd element (0-based index)
-    cout << "Third list element: " << *first_element << endl;
-
+    some_list.sort();
+    printList(some_list);
     return 0;
 
 }
