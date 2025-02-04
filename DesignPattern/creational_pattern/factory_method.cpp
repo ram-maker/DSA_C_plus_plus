10e7bd613c17c3286bfb8b7f6d5189ef29f439d0
ram-maker <ramprasadpoudel150@gmail.com>
2025-02-04 13:31:58 +0545

    chore:factory for word

--- DesignPattern/creational_pattern/factory_method.cpp
+++ DesignPattern/creational_pattern/factory_method.cpp
@@ -46,5 +46,8 @@ int main(){
     shared_ptr<DocumentFactory> document_factory=make_shared<PdfFactory>();
     shared_ptr<Document> document=document_factory->createDocument();
     document->renderPage();
+    shared_ptr<DocumentFactory> word_factory=make_shared<WordFactory>();
+    shared_ptr<Document> word=word_factory->createDocument();
+    word->renderPage();
     return 0;
 }
