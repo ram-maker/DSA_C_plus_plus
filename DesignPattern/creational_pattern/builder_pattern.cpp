7285215ad843ae31fab2ccb701fc8ac28cde8c1c
ram-maker <ramprasadpoudel150@gmail.com>
2025-02-04 19:47:18 +0545

    feat:method chaining

--- DesignPattern/creational_pattern/builder_pattern.cpp
+++ DesignPattern/creational_pattern/builder_pattern.cpp
@@ -2,107 +2,120 @@
 #include <cstring>
 #include <memory>
 using namespace std;
-class MediaFile{
-    bool is_audio=false;
-    bool is_video=false;
-    char *  audio_codec="";
-    char * video_codec="";
-    char * container="";
-    public:
-    void setIsAudio(bool is_image){
-        this->is_audio=is_image;
-    }
-    
-    void setIsVideo(bool is_video){
-        this->is_video=is_video;
+
+class MediaFile {
+    bool is_audio;
+    bool is_video;
+    char* audio_codec;
+    char* video_codec;
+    char* container;
+
+public:
+    // Constructor initializes pointers to nullptr to prevent garbage values
+    MediaFile() : audio_codec(nullptr), video_codec(nullptr), container(nullptr), is_audio(false), is_video(false) {}
+
+    void setIsAudio(bool is_audio) {
+        this->is_audio = is_audio;
     }
 
-    void setAudioCodec(char * audio_codec){
-        this->audio_codec=new char [strlen(audio_codec)+1];
-        strcpy(this->audio_codec,audio_codec);
+    void setIsVideo(bool is_video) {
+        this->is_video = is_video;
     }
 
-    void setVideoCodec(char * video_codec){
-          this->video_codec=new char [strlen(video_codec)+1];
-        strcpy(this->video_codec,video_codec);
+    void setAudioCodec(const char* audio_codec) {
+        this->audio_codec = new char[strlen(audio_codec) + 1];
+        strcpy(this->audio_codec, audio_codec);
     }
 
-    void setContainer(char * container){
-          this->container=new char [strlen(container)+1];
-        strcpy(this->container,container);
+    void setVideoCodec(const char* video_codec) {
+        this->video_codec = new char[strlen(video_codec) + 1];
+        strcpy(this->video_codec, video_codec);
     }
 
-    void display(){
-        cout<<"is_audio:"<<is_audio<<endl;
-        cout<<"is_video:"<<is_video<<endl;
-        cout<<"audio_codec:"<<audio_codec<<endl;
-        cout<<"video_codec:"<<video_codec<<endl;
-        cout<<"container:"<<container<<endl;
+    void setContainer(const char* container) {
+        this->container = new char[strlen(container) + 1];
+        strcpy(this->container, container);
     }
-    
+
+    void display() const {
+        cout << "is_audio: " << is_audio << endl;
+        cout << "is_video: " << is_video << endl;
+        cout << "audio_codec: " << (audio_codec ? audio_codec : "null") << endl;
+        cout << "video_codec: " << (video_codec ? video_codec : "null") << endl;
+        cout << "container: " << (container ? container : "null") << endl;
+    }
+
+    // ~MediaFile() {
+    //     delete[] audio_codec;
+    //     delete[] video_codec;
+    //     delete[] container;
+    // }
 };
 
-class MediaFileBuilder{
-    public:
-    virtual void buildIsAudio()=0;
-    virtual void buildIsVideo()=0;
-    virtual void buildAudioCodec()=0;
-    virtual void buildVideoCodec()=0;
-    virtual void buildContainer()=0;
-    virtual MediaFile build()=0;
-
+class MediaFileBuilder {
+public:
+    virtual MediaFileBuilder& buildIsAudio() = 0;
+    virtual MediaFileBuilder& buildIsVideo() = 0;
+    virtual MediaFileBuilder& buildAudioCodec() = 0;
+    virtual MediaFileBuilder& buildVideoCodec() = 0;
+    virtual MediaFileBuilder& buildContainer() = 0;
+    virtual MediaFile build() = 0;
 };
 
-class OpusCodecBuilder:public MediaFileBuilder{
-    private:
+class OpusCodecBuilder : public MediaFileBuilder {
+private:
     shared_ptr<MediaFile> media_file;
-    public:
-    OpusCodecBuilder(){
-        media_file=make_shared<MediaFile>();
+
+public:
+    OpusCodecBuilder() {
+        media_file = make_shared<MediaFile>();
     }
 
-    void buildIsAudio(){
+    MediaFileBuilder& buildIsAudio() override {
         media_file->setIsAudio(true);
+        return *this;  // return the builder itself for chaining
     }
 
-    void buildIsVideo(){
+    MediaFileBuilder& buildIsVideo() override {
         media_file->setIsVideo(false);
+        return *this;  // return the builder itself for chaining
     }
 
-    void buildAudioCodec(){
+    MediaFileBuilder& buildAudioCodec() override {
         media_file->setAudioCodec("opus");
+        return *this;  // return the builder itself for chaining
     }
 
-    void buildVideoCodec(){
+    MediaFileBuilder& buildVideoCodec() override {
         media_file->setVideoCodec("None");
+        return *this;  // return the builder itself for chaining
     }
 
-    void buildContainer(){
+    MediaFileBuilder& buildContainer() override {
         media_file->setContainer("opus");
+        return *this;  // return the builder itself for chaining
     }
 
-    MediaFile build(){
+    MediaFile build() override {
         return *media_file;
     }
-        
 };
 
-class MediaFileDirector{
-    public:
-    MediaFile constrcutMediaFile(OpusCodecBuilder &builder){
-        builder.buildIsAudio();
-        // builder.buildVideoCodec();
-        // builder.buildIsImage();
-        // builder.buildContainer();
-        return builder.build();
-
+class MediaFileDirector {
+public:
+    MediaFile constructMediaFile(OpusCodecBuilder& builder) {
+        return builder.buildAudioCodec()
+            //    .buildIsAudio()
+               .buildContainer()
+               .build();
+        
     }
 };
 
-int main(){
+int main() {
     OpusCodecBuilder opus_builder;
     MediaFileDirector director;
-    MediaFile media_file=director.constrcutMediaFile(opus_builder);
+    MediaFile media_file = director.constructMediaFile(opus_builder);
     media_file.display();
     return 0;
 }
