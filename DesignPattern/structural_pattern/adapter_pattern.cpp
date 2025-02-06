a01ee6a281a350c415315876967435f7c3476e92
ram-maker <ramprasadpoudel150@gmail.com>
2025-02-06 16:20:33 +0545

    feat:impl adapter system

--- DesignPattern/structural_pattern/adapter_pattern.cpp
+++ DesignPattern/structural_pattern/adapter_pattern.cpp
@@ -5,6 +5,7 @@ The adapter pattern generally consists of:
 adaptee: 
 This is generally a class that need to be adopted; Generally this class involve a legacy system , in which client has least control over.
 They involve a third party system, a library 
+It is a concrete type 
 
 Target:
 This is a interface that client expects, It defines method which makes adaptee compatible with client
@@ -16,5 +17,63 @@ Client:
 This is the client that leverage adapterclass to perform certain functionalities
 
 */
+#include <iostream>
+#include <memory>
+using namespace std;
 
+class CSVRendrer{
+    string name="";
+    string age="";
+    public:
+    CSVRendrer(){}
+    CSVRendrer(string name, string age){
+        this->name=name;
+        this->age=age;
+    }
 
+    std::string getCSV(){
+        return '"'+name+'"'+','+age;
+    }
+
+};
+
+class Renderer{
+    public:
+    virtual void render()=0;
+};
+
+class RendererAdpater:public Renderer{
+    CSVRendrer csv;
+    string name="";
+    string age="";
+    public:
+    RendererAdpater(){
+
+    }
+
+    RendererAdpater(CSVRendrer &csv_renderer){
+        this->csv=csv_renderer;
+    }
+
+    void render(){
+        string csv_string=csv.getCSV();
+        int start=0;
+        int end=csv_string.find(",");
+        name=csv_string.substr(start,end);
+        start=end+1;
+        age=csv_string.substr(start);        
+    }
+
+    std::string getJson(){
+         return "{\"" + name + "\":\"" + age + "\"}";
+    }
+};
+
+int main(){
+    CSVRendrer csv_render("Ram","24");
+    RendererAdpater json_render(csv_render);
+    json_render.render();
+    string json_string=json_render.getJson();
+    cout<<"Json String:"<<json_string<<endl;
+    return 0;
+}
