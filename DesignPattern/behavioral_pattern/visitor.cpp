8132ade020c8be9af85fc5e7ff4dade528776aeb
ram-maker <ramprasadpoudel150@gmail.com>
2025-02-10 09:37:16 +0545

    chore:implement visitor method for circumference calculation

--- DesignPattern/behavioral_pattern/visitor.cpp
+++ DesignPattern/behavioral_pattern/visitor.cpp
@@ -7,62 +7,109 @@ It changes the behaviour of classes without modifying it's structure.
 It works in the process of double dispatch
 The most primary use case of visitor is found in node like structure
 
+Generally visitor pattern is defined in node like structure having common interface, since all the node more or less share some characteristics, 
+a reqiurement may be there to change each node functionalities, so thats the reaso why each node are forced with accept moethod? Rather tha n just 
+implementing a method on each node, its more like because of use case in which visitor is leveraged for
+
+The core logic lies in visitor class rather than in element class:perfectly separting the concerns
+
 Element:
 Concrete type in which modifcation is reqiured
+Visitor:
+The class that visits the concreteclasses
+
+I am predefined element existing in a node, now i want to calculate circumference of each class,
 
 */
 #include <iostream>
 #include <memory>
 using namespace std;
-class ShapeVisitor;
-class Visitor;
-class ShapeRenderer{
-    public:
-    virtual void renderer()=0;
-    virtual void accept(shared_ptr<Visitor> visitor)=0;
-};
-/*Element*/
-class CircleRenderer:public ShapeRenderer{
-    int radius;
-    public:
-    CircleRenderer(int radius){
-        this->radius=radius;
-    }
-    void renderer(){
-        cout<<"Rendering PDF, radius="<<radius<<endl;
-    }
-    void accept(shared_ptr<Visitor> visitor){
-        visitor->visit(*this);
-    }
-};
-
-class RectangleRenderer:public ShapeRenderer{
-    int length, breadth;
-    public:
-    RectangleRenderer(int length, int breadth){
-        this->length=length;
-        this->breadth=breadth;
-    }
-    void renderer(){
-        cout<<"Rendering,length="<<length<<",breadth="<<breadth<<endl;
-    }
-    void accept(shared_ptr<Visitor> visitor){
-        visitor->visit(*this);
-    }
-};
+class CircleRenderer;
+class RectangleRenderer;
 
 class Visitor{
     public:
-    virtual void visit(shared_ptr<CircleRenderer> circle_visitor)=0;
-    virtual void visit(shared_ptr<RectangleRenderer> rectangle_visitor)=0;
+    virtual void visit(CircleRenderer &circle_visitor)=0;
+    virtual void visit(RectangleRenderer &rectangle_visitor)=0;
 };
 
-class ModifyShapeVisitor:public Visitor{
+/*Element Interface*/
+class ShapeRenderer{
     public:
-    void visit(shared_ptr<CircleRenderer> circle_visitor){
+    virtual void renderer()=0;
+    virtual void accept(Visitor &visitor)=0;
+};
 
+/*Concrete Element 1*/
+class CircleRenderer : public ShapeRenderer {
+    int radius;
+
+public:
+    CircleRenderer(int radius) {
+        this->radius = radius;
     }
-    void visit(shared_ptr<RectangleRenderer> rectangle_visitor){
 
+    void renderer() override {
+        cout << "Rendering Circle, radius=" << radius << endl;
+    }
+
+    void accept(Visitor& visitor) override {  // Implementing the method
+        visitor.visit(*this);
+    }
+
+    int getRadius() {
+        return radius;
     }
 };
+
+/*Concrete Element 2*/
+class RectangleRenderer : public ShapeRenderer {
+    int length, breadth;
+
+public:
+    RectangleRenderer(int length, int breadth) {
+        this->length = length;
+        this->breadth = breadth;
+    }
+
+    void renderer() override {
+        cout << "Rendering, length=" << length << ", breadth=" << breadth << endl;
+    }
+
+    void accept(Visitor& visitor) override {  // Implementing the method
+        visitor.visit(*this);
+    }
+
+    int getLength() {
+        return length;
+    }
+
+    int getBreadth() {
+        return breadth;
+    }
+};
+
+
+
+
+class CircumferenceVisitor:public Visitor{
+    public:
+    void visit(CircleRenderer& circle_visitor){
+        int radius=circle_visitor.getRadius();
+        float circumference=2*3.14*radius;
+        cout<<"The circumference of a circle is:"<<circumference<<endl;
+    }
+    void visit(RectangleRenderer& rectangle_visitor){
+        int breadth=rectangle_visitor.getBreadth();
+        int length=rectangle_visitor.getLength();
+        cout<<"The circumference of a rectangle is is:"<<2*(breadth+length)<<endl;
+    }
+};
+
+ int main(){
+    shared_ptr<ShapeRenderer> circle_renderer=make_shared<CircleRenderer>(3);
+    shared_ptr<ShapeRenderer> rectangle_renderer=make_shared<RectangleRenderer>(4,5);
+    shared_ptr<Visitor>visitor=make_shared<CircumferenceVisitor>();
+    circle_renderer->accept(*visitor);
+    rectangle_renderer->accept(*visitor);
+ }
